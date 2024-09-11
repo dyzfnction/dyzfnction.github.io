d87d409f844e1b0376978a50b1ab5fc16adf6aad
Seth <kimpeses@gmail.com>
2024-09-11 02:30:42 +0200

    Add files via upload

--- js/star.js
+++ js/star.js
@@ -1,1 +1,23 @@
+function firstPage(){
+    location.href = '../site/index.html' ; /* Lien vers la page d'accueil */
+}
 
+function mesProjets(){
+	location.href = '../site/mesProjets.html'; /* Lien vers la page de mes projets. */
+}
+
+function quiSuisJe(){
+    location.href = '../site/whoamI.html' ; /* Lien vers la page de présentation de mon parcours scolaire. */
+}
+
+function hobbyPassion(){
+    location.href = "../site/hobbyPassion.html" ; /* Lien vers la page de mes hobby/passions */
+}
+
+function projFormation(){
+    location.href = '../site/projetFormation.html' ; /* Lien vers la page de mon projet de formation. */
+}
+
+// function maVeille(){
+ //   location.href = '../site/dossierVeille.html' ; /* Lien vers la page de dossier de veille */
+//} 
