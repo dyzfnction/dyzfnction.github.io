603f014706fa75869fc990ee883d9b536ee5d4d4
Seth <kimpeses@gmail.com>
2024-11-21 21:01:59 +0100

    Update star.js

--- js/star.js
+++ js/star.js
@@ -18,6 +18,53 @@ function projFormation(){
     location.href = '../site/projetFormation.html' ; /* Lien vers la page de mon projet de formation. */
 }
 
-// function maVeille(){
- //   location.href = '../site/dossierVeille.html' ; /* Lien vers la page de dossier de veille */
-//} 
+
+// VEILLE TECHNO
+
+function accueil(){
+	location.href = '../site/vtaccueil.html'; /* Lien vers la page d'accueil. */
+}
+
+function introduction(){
+	location.href = '../site/vtintro.html'; /* Lien vers la page d'introduction. */
+}
+
+function a1(){
+	location.href = '../site/vta1.html' ; /* Lien vers le premier article. */
+}
+
+function a2(){
+	location.href = '../site/vta2.html' ; /* Lien vers le deuxième article. */
+}
+
+function a3(){
+	location.href = '../site/vta3.html' ; /* Lien vers le troisième article. */
+}
+
+function a4(){
+	location.href = '../site/vta4.html' ; /* Lien vers le quatrième article. */
+}
+
+function a5(){
+	location.href = '../site/vta5.html' ; /* Lien vers le cinquième article. */
+}
+
+function a6(){
+	location.href = '../site/vta6.html' ; /* Lien vers le sixième article. */
+}
+
+function a7(){
+	location.href = '../site/vta7.html' ; /* Lien vers le septième article. */
+}
+
+function a8(){
+	location.href = '../site/vta8.html' ; /* Lien vers le septième article. */
+}
+
+function a9(){
+	location.href = '../site/vta9.html' ; /* Lien vers le septième article. */
+}
+
+function recap(){
+	location.href = '../site/vtrecap.html'; /* Lien vers le récapitulatif. */
+}
